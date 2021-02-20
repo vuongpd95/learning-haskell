b9eb3aef4b8cb2a65ae244d1b1d72c8ce84999d4
Vuong Pham <vuongpd95@gmail.com>
2021-02-21 01:49:19 +0700

    Add chapter 3 exercises

---
+++ chapter-3/ChangeMood.hs
@@ -0,0 +1,7 @@
+module ChangeMood where
+
+data Mood = Blah | Woot deriving Show
+
+reverse :: Mood -> Mood
+reverse Blah = Woot
+reverse    _ = Blah
---
+++ chapter-3/print2.hs
@@ -0,0 +1,8 @@
+module Print where
+
+main :: IO ()
+main = do
+  putStrLn "Count to four for me:"
+  putStr   "one, two"
+  putStr   ", three, and"
+  putStrLn " four!"
