7b7ff7fc08bc9a4e12f750cf895545029ab8f2e6
Vuong Pham <vuongpd95@gmail.com>
2021-03-12 04:12:30 +0700

    Add PoemLines

---
+++ chapter-9/PoemLines.hs
@@ -0,0 +1,45 @@
+module PoemLines where
+
+firstSen = "Tyger Tyger, burning bright\n"
+
+secondSen = "In the forests of the night\n"
+
+thirdSen = "What immortal hand or eye\n"
+
+fourthSen =
+  "Could frame thy fearful\
+  \ symmetry?"
+
+sentences = firstSen ++ secondSen ++ thirdSen ++ fourthSen
+
+-- putStrLn sentences -- should print
+-- Tyger Tyger, burning bright
+-- In the forests of the night
+-- What immortal hand or eye
+-- Could frame thy fearful symmetry?
+
+wordsWhen     :: (Char -> Bool) -> String -> [String]
+wordsWhen p s =  case dropWhile p s of
+                      "" -> []
+                      s' -> w : wordsWhen p s''
+                            where (w, s'') = break p s'
+
+myLines :: String -> [String]
+myLines = wordsWhen (== '\n')
+
+
+-- What we want 'myLines sentences'
+-- to equal
+shouldEqual =
+  [ "Tyger Tyger, burning bright",
+    "In the forests of the night",
+    "What immortal hand or eye",
+    "Could frame thy fearful symmetry?"
+  ]
+
+-- The main function here is a small test
+-- to ensure you've written your function
+-- correctly.
+main :: IO ()
+main =
+  print $ "Are they equal? " ++ show (myLines sentences == shouldEqual)
