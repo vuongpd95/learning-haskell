07a1c605ea3004b29adeee3012a103949f86ca21
Vuong Pham <vuongpd95@gmail.com>
2021-03-12 03:41:53 +0700

    Remove redundant brackets

--- chapter-9/myWords.hs
+++ chapter-9/myWords.hs
@@ -2,4 +2,4 @@ myWords :: [Char] -> [[Char]]
 myWords sentence = go sentence []
   where go x results
           | x == ""   = reverse results
-          | otherwise = go (drop 1 (dropWhile (/= ' ') x)) ((takeWhile (/= ' ') x):results)
+          | otherwise = go (drop 1 (dropWhile (/= ' ') x)) (takeWhile (/= ' ') x : results)
